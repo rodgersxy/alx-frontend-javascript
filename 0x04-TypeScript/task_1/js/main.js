0fa83c1d778f2ee022809232a415ce53ca4cd53e
rodgersxy <rodgersny99@gmail.com>
2024-12-13 10:50:21 +0300

    add: 4. Writing a class

--- 0x04-TypeScript/task_1/js/main.js
+++ 0x04-TypeScript/task_1/js/main.js
@@ -1,6 +1,19 @@
 var printTeacher = function (firstName, lastName) {
     return firstName.charAt(0) + ". " + lastName;
 };
+var StudentClass = /** @class */ (function () {
+    function StudentClass(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    StudentClass.prototype.workOnHomework = function () {
+        return 'Currently working';
+    };
+    StudentClass.prototype.displayName = function () {
+        return this.firstName;
+    };
+    return StudentClass;
+}());
 // usage
 // const director1: Directors = {
 //     firstName: 'John',
@@ -12,4 +25,7 @@ var printTeacher = function (firstName, lastName) {
 //     yearsOfExperience: 5,
 // };
 // console.log(director1);
-console.log(printTeacher('John', 'Doe'));
+// console.log(printTeacher('John', 'Doe'));
+var student1 = new StudentClass('John', 'Doe');
+console.log(student1.workOnHomework());
+console.log(student1.displayName());
--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,6 +18,31 @@ const printTeacher: printTeacherFunction = (firstName, lastName) => {
     return `${firstName.charAt(0)}. ${lastName}`;
 };
 
+interface StudentConstructor {
+    new (firstName: string, lastName: string): StudentClassInterface;
+}
+interface StudentClassInterface {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+    displayName(): string {
+        return this.firstName;
+    }
+}
+
 // usage
 // const director1: Directors = {
 //     firstName: 'John',
@@ -29,4 +54,8 @@ const printTeacher: printTeacherFunction = (firstName, lastName) => {
 //     yearsOfExperience: 5,
 // };
 // console.log(director1);
-console.log(printTeacher('John', 'Doe'));
+// console.log(printTeacher('John', 'Doe'));
+
+const student1 = new StudentClass('John', 'Doe');
+console.log(student1.workOnHomework());
+console.log(student1.displayName());
